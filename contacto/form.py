b643a53cfb1973e5957fcacf3648cb9469c7b76f
ariosmen <arielosmen@hotmail.com>
2023-08-21 18:46:53 -0300

    Contacto corregido

--- contacto/form.py
+++ contacto/form.py
@@ -2,8 +2,8 @@ from django import forms
 
 class FormularioContacto(forms.Form):
     
-    asunto = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Asunto'}))
     nombre = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Nombre'}))
+    asunto = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Asunto'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email'}))
     mensaje = forms.CharField(max_length=3000, widget=forms.Textarea(attrs={'placeholder': 'Mensaje'}))
 
